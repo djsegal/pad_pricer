7a178ef39aa62b9f554c558fb28fb56d9a4e9191
Dan Segal <dansegal2@gmail.com>
2020-01-16 22:06:44 -0800

    Clean up utils file (set as a list of one-liners)

--- js/utils.js
+++ js/utils.js
@@ -1,39 +1,18 @@
 const average = (array) => array.reduce((a, b) => a + b) / array.length;
-const custom_transpose = array => array[0].map((r, i) => array.map(c => c[i]));
+const customTranspose = array => array[0].map((r, i) => array.map(c => c[i]));
 
-function _XClean(XData) {
-    var values = Object.keys(XData).map(function(curKey){
-        return Object.keys(XData[curKey]).map(function(subKey){
-            return XData[curKey][subKey];
-        });
+function camelize(text) {
+    return text.replace(/^([A-Z])|[\s-_]+(\w)/g, function(match, p1, p2, offset) {
+        if (p2) return p2.toUpperCase();
+        return p1.toLowerCase();
     });
-
-    return custom_transpose(values);
 }
 
-function XClean(XData) {
-  return array2mat(_XClean(XData));
-};
+function decamelize(str, separator){
+  separator = typeof separator === 'undefined' ? '_' : separator;
 
-function yClean(yData) {
-  return Object.keys(yData).map(function(curKey){
-      return Math.log10(yData[curKey]);
-  });
-};
-
-function R2(curRidge, XData, yData) {
-  yAverage = average(yData);
-  yPredict = curRidge.predict(XData);
-
-  ssRes = 0.0
-  ssTot = 0.0
-
-  for (var i = 0; i < XData.length; i++) {
-    ssRes += Math.pow( yData[i] - yPredict[i] , 2 )
-    ssTot += Math.pow( yData[i] - yAverage , 2 )
-  }
-
-  curScore = 1 - ssRes / ssTot;
-
-  return curScore;
+  return str
+        .replace(/([a-z\d])([A-Z])/g, '$1' + separator + '$2')
+        .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + separator + '$2')
+        .toLowerCase();
 }
